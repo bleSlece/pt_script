9a554e518460b4a52a9363be04ee57367a5a0fdb
qwerty33 <116711972+bleSlece@users.noreply.github.com>
2023-07-29 16:27:00 +0800

    Create GGn_Repost_check.js

---
+++ GGn_Repost_check.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://gazellegames.net/torrents.php?searchstr=&artistname=PlayStation+4&order_by=time&order_way=desc
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=gazellegames.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();

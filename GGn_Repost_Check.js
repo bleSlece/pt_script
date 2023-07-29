73594b3fa635a6d24378f0199f4e0bf22fe309ba
qwerty33 <116711972+bleSlece@users.noreply.github.com>
2023-07-29 17:41:06 +0800

    Update GGn_Repost_Check.js

--- GGn_Repost_Check.js
+++ GGn_Repost_Check.js
@@ -7,21 +7,20 @@
 // @match        https://gazellegames.net/torrents.php*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=gazellegames.net
 // @require      https://code.jquery.com/jquery-3.7.0.min.js
-// @grant        none
+// @grant        unsafeWindow
 // ==/UserScript==
 
 (function() {
     'use strict';
     // Your code here...
-
-    var a = $('.group')
     
     var search_anchor = 'https://pterclub.com/torrents.php?tag_exclusive=&tag_internal=&tag_mandarin=&tag_cantonese=&tag_doityourself=&tag_master=&cat409=1&incldead=0&spstate=0&inclbookmarked=0&check=&helper=&search=SearchAnchor&search_area=0&search_mode=0'
-    a.each(function (index) {
-        var gp = $(this).find('#groupname')
+    $('.group').each(function (index, element) {
+        var gp = $(element).find('#groupname')
         var game_name = gp.text()
-        var search_link = search_anchor.replace('SearchAnchor', game_name)
-        var search_html = '<span id="hyphen">---</span> <span><a href="'+search_link+'" target="_blank">Search</a></span>'
+        var search_name = game_name.replaceAll('&', '').replaceAll(' ', '+')
+        var search_link = search_anchor.replace('SearchAnchor', search_name)
+        var search_html = ' <span>-</span> <span><a href="'+search_link+'" target="_blank">Search</a></span>'
         gp.after(search_html)
     })
 
